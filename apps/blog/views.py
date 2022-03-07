2c581496d0cabaa2c8160be2c6434e39f127b87b
Toktosun <suksurali00@gmail.com>
2022-03-07 19:49:18 +0600

    alternatives for views;

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,15 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from apps.blog.models import Article
+
+# альтернатива для ArticleListView
+# def show_article_list_view(request):
+#     article_qs = Article.objects.all()  # из базы достаём все публикации
+#     response = render(request, 'blog.html',
+#                       context={'article_list': article_qs})
+#     return response
 
 
 class ArticleListView(ListView):
@@ -9,6 +17,7 @@ class ArticleListView(ListView):
     queryset = Article.objects.all()
     template_name = 'blog.html'
     context_object_name = 'article_list'
+    paginate_by = 9
 
 
 class ArticleDetailView(DetailView):
@@ -16,3 +25,18 @@ class ArticleDetailView(DetailView):
     template_name = 'blog-single.html'
     model = Article
     slug_field = 'pk'
+    context_object_name = 'article'
+
+
+# альтернатива для ArticleDetailView
+# class ArticleDetailTemplateView(TemplateView):
+#     template_name = 'blog-single.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = dict()
+#         article_pk = kwargs['pk']  # достали из url
+#         try:
+#             context['article'] = Article.objects.get(id=article_pk)
+#         except Article.DoesNotExist:
+#             raise Http404
+#         return context
